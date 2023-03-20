4585fbbd2486607817e756ebf152e8b517d93e05
eSSeGH <ste.virgadaula@gmail.com>
2023-03-20 16:16:20 +0100

    FEAT info added to displayed table

--- index.php
+++ index.php
@@ -48,22 +48,64 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP-hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
 
-<?php 
+    <header style="height: 90px; background-color: rgba(0, 0, 0, 0.95)">
+        <h2 class="text-center" style="line-height: 90px; color: coral;">BoolNB</h2>
+    </header>
 
-foreach ($hotels as $hotel) {
+    <main class="pt-5" style="height: calc(100vh - 90px); background-color: coral;">
 
-    foreach ($hotel as $value) {
+        <div class="container">
 
-    echo $value;
-    echo '<br>';
+            <table class="table">
 
-    }
-}
-?>
-    
+                <thead>
+                    <tr>
+
+                        <?php
+                        foreach ($hotels[0] as $key => $value) {
+                            ?>
+                                <th scope="col"><?php  echo $key; ?></th>
+                            <?php
+                        }
+                        ?>
+
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php
+                    foreach ($hotels as $hotel) {
+                        ?>
+
+                        <tr>
+
+                            <?php
+                            foreach ($hotel as $key => $value) {
+                                ?>
+                                    <td><?php  echo $value; ?></td>
+                                <?php
+                            }
+                            ?>
+
+                        </tr>
+
+                        <?php
+                    }
+                    ?>
+
+                </tbody>
+
+            </table>
+
+        </div>
+
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
